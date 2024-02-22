822a179c7e3c8fe84b096d71e3c57bc12297fc0e
Aryan Saxena <afteraryan@gmail.com>
2024-02-22 18:37:22 +0530

    Tank Spawner Created

---
+++ CharacterSelection-main/Assets/Sprites/TankSpawner.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject tankPrefab;
+
+    private void Start()
+    {
+        Instantiate(tankPrefab, transform.position, Quaternion.identity);
+    }
+}
